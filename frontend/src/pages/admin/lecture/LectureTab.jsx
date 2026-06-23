7841e9fb71c6e1998929916e8379fdf248b89d0a
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-23 12:36:11 +0530

    updated code

--- frontend/src/pages/admin/lecture/LectureTab.jsx
+++ frontend/src/pages/admin/lecture/LectureTab.jsx
@@ -18,7 +18,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const MEDIA_API = "https://lms-deploy-lejt.onrender.com/api/v1/media";
 
 const LectureTab = () => {
     const [lectureTitle, setLectureTitle] = useState("");
